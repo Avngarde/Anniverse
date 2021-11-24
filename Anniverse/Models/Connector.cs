60986c0abc8033a59c4bab5bdccf537f2fad5ddf
ShamanDev <pivotguy888@gmail.com>
2021-11-25 00:10:44 +0100

    Created Connector and Event Models

---
+++ Anniverse/Models/Connector.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Anniverse.Models
+{
+    public class Connector
+    {
+        public ObservableCollection<Event> events;
+
+        public Connector()
+        {
+            events = new ObservableCollection<Event>();
+            if (!CheckIfEventsFileExists())
+            {
+                CreateEventsFile();
+            }
+        }
+
+        private bool CheckIfEventsFileExists()
+        {
+            return File.Exists(@"./events/events.json");
+        }
+
+        private void CreateEventsFile()
+        {
+            Directory.CreateDirectory("./events/");
+            WriteToFile();
+        }
+
+        public void ReadEvents()
+        {
+            string jsonString = File.ReadAllText(@"./events/events.json");
+            this.events = JsonSerializer.Deserialize<ObservableCollection<Event>>(jsonString);
+        }
+
+        public ObservableCollection<Event> GetEvents()
+        {
+            ReadEvents();
+            return events;
+        }
+
+        public void AddNewEvent(Event e)
+        {
+            this.events.Add(e);
+        }
+
+        public void WriteToFile()
+        {
+            string serializedEvents = JsonSerializer.Serialize(this.events);
+            File.WriteAllText(@"./events/events.json", serializedEvents);
+        }
+    }
+}
---
+++ Anniverse/Models/Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anniverse.Models
+{
+    public class Event
+    {
+        public enum Type
+        {
+            Birthday,
+            Deadline,
+            Anniversary,
+            Monthly,
+            Meeting,
+            Other
+        }
+
+        public String ProgressBarColour { get; set; }
+
+        public byte Id { get; set; }
+        public Type EventType { get; set; }
+        public String? Name { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
