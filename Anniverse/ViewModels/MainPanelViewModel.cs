73842347e89f85eb99495dc52c7b040d1628a121
ShamanDev <pivotguy888@gmail.com>
2021-11-25 00:11:50 +0100

    Added fields to MainPanelViewModel

--- Anniverse/ViewModels/MainPanelViewModel.cs
+++ Anniverse/ViewModels/MainPanelViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Anniverse.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,8 @@ namespace Anniverse.ViewModels
 {
     class MainPanelViewModel : ViewModelBase
     {
+        public string CurrentDate => "Current Date: " + DateTime.Today.ToString("dd/MM/yyyy");
+
+        public ObservableCollection<Event> Events => new Connector().GetEvents();
     }
 }
