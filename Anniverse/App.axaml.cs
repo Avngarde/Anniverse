d94be5f442fc998a50caac60661394143d89bc56
ShamanDev <pivotguy888@gmail.com>
2021-11-24 23:40:26 +0100

    Dodaj pliki projektów.

---
+++ Anniverse/App.axaml.cs
@@ -0,0 +1,29 @@
+using Anniverse.ViewModels;
+using Anniverse.Views;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+namespace Anniverse
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
